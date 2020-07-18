59f2e3d9150ed3f92712ca2d2a4fb85ef5fa345a
Ankit Gupta <i.ankitgupta25@gmail.com>
2020-07-18 23:36:11 +0530

    changes added

--- smartstudy/urls.py
+++ smartstudy/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('SmartstudySquare', include('SmartstudySquare.urls')),
-    url(r'^$', views.index, name='index'),]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    url(r'^$', views.index, name='index'),]
