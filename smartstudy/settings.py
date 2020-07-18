b487e2f35ba73ee9de0b94d58d7a937076e616f3
Ankit Gupta <i.ankitgupta25@gmail.com>
2020-07-18 20:10:52 +0530

    changes added

--- smartstudy/settings.py
+++ smartstudy/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "root")
 
-STATIC_URL = '/static/'
+STATIC_URL = '/SmartstudySquare/static/'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')]
+        os.path.join(BASE_DIR, 'SmartstudySquare/static')]
 
-STATICFILES_FINDERS= (
+STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
